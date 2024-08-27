5b807d4cd677497b95bf81fca15b5da5c20a1700
nnn <bonbi0604@gmail.com>
2024-08-27 15:36:18 +0900

    수정

--- server/FindUnlockEpi.js
+++ server/FindUnlockEpi.js
@@ -4,11 +4,17 @@ const FindUnlockEpi = async(id, fairy_no) =>{
     let result;
     [result] = await pool.query('SELECT * FROM progress WHERE id =?  AND fairy_no = ? ORDER BY epi_no DESC limit 1', [id, fairy_no]);
     
-    if(result){
-        return { epi_no: 0 }
-    }else{
-        return {epi_no: result.epi_no}
+    if(result.length===0){
+        return { 
+            fairy_no: 0,
+            total_episode : 0
+         }
     }
+    const [total_episode] = await pool.query('SELECT COUNT(*) FROM episode WHERE fairy_no=?', [fairy_no]);
+        return {
+            fairy_no: result.fairy_no,
+            total_episode : total_episode.count
+        }
 }
 module.exports ={
     FindUnlockEpi

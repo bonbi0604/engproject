6da00e3bca53127f01eb182bd6147d90ac15cb8a
syhong <wendy1009@naver.com>
2024-09-25 17:55:30 +0900

    .

--- server/register.js
+++ server/register.js
@@ -11,18 +11,6 @@ const addUser = async (id, passwd) => {
     // 비밀번호 해싱
     const crypt_passwd = crypto.pbkdf2Sync(passwd, salt, 1000, 64, 'sha512').toString('hex');
 
-<<<<<<< HEAD
-    const [result] = await pool.query(
-        'INSERT INTO user (id, passwd, salt) VALUES (?, ?, ?)',
-        [id, crypt_passwd, salt]
-    );
-    //회원가입 시, 기본으로 해금되도록 progress테이블에 값 추가
-    pool.query(
-        'INSERT INTO progress(id, title, epi_no, fairy_no) VALUES (?, ?, ?, ?)',
-        [id, 'The Three Little Pigs',1,1]
-    )
-    return { insertId: result.insertId, id, passwd };
-=======
     try {
         // 사용자 정보를 데이터베이스에 추가
         const [result] = await pool.query(
@@ -41,7 +29,6 @@ const addUser = async (id, passwd) => {
         console.error('Error inserting user:', error);
         throw new Error('Failed to add user');
     }
->>>>>>> a0fe8f7 (수정)
 };
 
 module.exports = {

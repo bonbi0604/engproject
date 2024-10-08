f48794ed1add0519e7172eb4879c2163e0de6f8c
bonbi <bonbi@naver.com>
2024-10-08 14:36:04 +0900

    sendWord 리스트값만 return하도록 만듦.

--- server/sendWord.js
+++ server/sendWord.js
@@ -1,16 +1,15 @@
 const pool = require('./db');
 
 const sendWord = async(fairy_no, epi_no) =>{
-    
-    const [result] = await pool.query('SELECT eng FROM words WHERE  fairy_no =? AND epi_no = ?', [fairy_no, epi_no]);
+    const [result] = await pool.query('SELECT eng FROM words WHERE fairy_no = ? AND epi_no = ?', [fairy_no, epi_no]);
+    list = [];
     if (result.length === 0) {
-        return {
-            fairy_no: null,
-            total_episode : null
-        }
+        return null;
     }
-    console.log([result]);
-    return [result];
+    for(i=0;i < result.length;i++){
+        list.push(result[i].eng);
+    }
+    return list;
 }
 
 
